e4dcb3f03adcc1a90a0dbe92d3747fac26a271b1
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2024-03-25 21:26:30 +0700

    add eventListener : onClick

--- app/src/components/Button/Button.jsx
+++ app/src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import "./Button.css";
 
 /* eslint-disable react/prop-types */
-export default function Button({ type, value, disabled }) {
+export default function Button({ type, value, disabled, onClick }) {
   return (
-    <button type={type} disabled={disabled}>
+    <button type={type} disabled={disabled} onClick={onClick}>
       {value}
     </button>
   );
