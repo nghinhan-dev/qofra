4aaf083ac1433bc1790fe1da347e39797811a4bf
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2024-03-30 13:25:56 +0700

    API : create

--- server/controllers/finding.mjs
+++ server/controllers/finding.mjs
@@ -1,28 +1,63 @@
 import Finding from "../models/Finding.mjs";
-import { uploadImg } from "../lib/sharp.mjs";
+import User from "../models/User.mjs";
+import { uploadImages } from "../lib/firebase.mjs";
+import { dueDateCalculator } from "../lib/date-fns.mjs";
+import { sendMail } from "../lib/nodeMailer.mjs";
 import { createError } from "../utils/createError.mjs";
-import fs from "fs";
+// import { createError } from "../utils/createError.mjs";
+// import fs from "fs";
 
 export async function createFinding(req, res, next) {
   try {
-    await uploadImg(req.file);
-    res.status(200).send({ message: "Uploaded" });
+    const { questionID, scope, process, picID, desc } = req.body;
+    let results;
+    if (req.files.length !== 0) {
+      results = await uploadImages(req.files);
+    } else {
+      results = [];
+    }
+
+    const pic = await User.findById(picID);
+    if (!pic) throw createError(404, "Cannot find person in charge");
+
+    const foundDate = new Date();
+    const dueDate = dueDateCalculator(Date.now(), scope);
+
+    const newFinding = new Finding({
+      question: questionID,
+      foundDate: foundDate,
+      dueDate: dueDate,
+      reporter: req.user._id,
+      personInCharge: pic._id,
+      desc: desc,
+      images: results,
+      status: "Ongoing",
+    });
+
+    await sendMail(
+      req.user.fullName,
+      pic,
+      { foundDate, dueDate, desc },
+      { process, scope }
+    );
+
+    // await newFinding.save();
+
+    res.status(200).send({ message: "Uploaded", finding: newFinding });
   } catch (error) {
-    console.log("error:", error);
-    res.status(400).send({ error: "error" });
+    next(error);
   }
 }
 
 export async function getFindings(req, res, next) {
   try {
-    fs.readFile(
-      `/dev web/qofra/server/data/images/37492752-50df-446b-9ba7-6ca332ee459c.jpg`,
-      "base64",
-      (err, data) => {
-        res.status(200).send({ content: data });
-      }
-    );
+    const findingId = req.params.findingID;
+
+    const finding = await Finding.findById(findingId);
+
+    res.send({ finding: finding });
   } catch (error) {
+    console.log("error:", error);
     next(error);
   }
 }
--- server/routes/finding.mjs
+++ server/routes/finding.mjs
@@ -1,9 +1,13 @@
 import express from "express";
 import { upload } from "../lib/multer.mjs";
 import { createFinding, getFindings } from "../controllers/finding.mjs";
+import { verifyToken } from "../middlewares/auth/verifyToken.mjs";
+import { fileValidate } from "../middlewares/validation/fileValidate.mjs";
 
 export const findingRoutes = express.Router();
 
 findingRoutes
-  .post("/postImages", upload.single("findingImage"), createFinding)
-  .get("/getImages", getFindings);
+  .use(verifyToken)
+  .post("/create", upload.array("findingImages"), fileValidate, createFinding)
+  .get("/getImages", getFindings)
+  .get("/finding/:findingID", getFindings);
