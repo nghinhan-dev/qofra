90d6b4b5832e9f5c1b7869f7d24ce361fbfe5f58
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2024-04-12 15:05:16 +0700

    chartjs

--- app/src/pages/Report/Report.jsx
+++ app/src/pages/Report/Report.jsx
@@ -1,3 +1,83 @@
+import {
+  Chart as ChartJS,
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController,
+} from "chart.js";
+import { Chart } from "react-chartjs-2";
+import faker from "faker";
+
+ChartJS.register(
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController
+);
+
+const options = {
+  plugins: {
+    title: {
+      display: true,
+      text: "Chart.js Bar Chart - Stacked",
+    },
+  },
+  responsive: true,
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true,
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      type: "line",
+      label: "Dataset 4",
+      borderColor: "rgb(234, 234, 29)",
+      borderWidth: 2,
+      fill: false,
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+    },
+    {
+      label: "Dataset 1",
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      backgroundColor: "rgb(255, 99, 132)",
+    },
+    {
+      label: "Dataset 2",
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      backgroundColor: "rgb(75, 192, 192)",
+    },
+    {
+      label: "Dataset 3",
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      backgroundColor: "rgb(53, 162, 235)",
+    },
+  ],
+};
+
 export default function Report() {
-  return <div>Report</div>;
+  return (
+    <section className="d__flex">
+      <Chart type="bar" options={options} data={data} />
+    </section>
+  );
 }
