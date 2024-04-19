3b2c14e81bbaa76b4b7e7995fb9f1c999a423726
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2024-04-19 09:39:52 +0700

    minor styles

--- app/src/components/Footer/Footer.jsx
+++ app/src/components/Footer/Footer.jsx
@@ -2,10 +2,11 @@ export default function Footer() {
   return (
     <footer>
       <p>
-        Copyright &#xa9; 2024,{" "}
+        {/* Copyright &#xa9; 2024,{" "}
         <a href="https://github.com/nghinhan-dev" target="_blank">
           sillywhale
-        </a>
+        </a> */}
+        Copyright framas &reg;
       </p>
     </footer>
   );
