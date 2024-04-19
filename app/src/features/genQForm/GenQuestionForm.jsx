3ae509eb807e6c797cb82ff0b3a79c379392eca9
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2024-04-19 09:41:01 +0700

    fix wrong initial LevelOpts

--- app/src/features/genQForm/GenQuestionForm.jsx
+++ app/src/features/genQForm/GenQuestionForm.jsx
@@ -16,10 +16,9 @@ export default function GenQuestion({ setDepartment, setProcess, setLevel }) {
     { value: "Crushing", label: "Production" },
   ]);
   const [levelOptions, setLevelOpts] = useState([
-    { value: "Mold Setter", label: "Mold Setter" },
-    { value: "Extruder", label: "Extruder" },
-    { value: "Mixing", label: "Mixing" },
-    { value: "Crushing", label: "Production" },
+    { value: 1, label: "1" },
+    { value: 2, label: "2" },
+    { value: 3, label: "3" },
   ]);
 
   const deparmentSelect = (value) => {
